324684dd8f1f5ecabd16877b100e431978d2a3ed
noname <none@none.com>
2025-07-01 15:06:36 +0900

    Initial implementation of Zegop w/ UT

--- Project18/main.cpp
+++ Project18/main.cpp
@@ -3,11 +3,24 @@
 class Cal {
 public:
 	// 이곳에 코드 작성
+	int getZegop(int a) {
+		return a * a;
+	}
 };
 
 //테스트 케이스 작성
-TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+TEST(conflicttest, zegoptest1) {
+	Cal cal;
+	int expected = 9;
+	int actual = cal.getZegop(3);
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(conflicttest, zegoptest2) {
+	Cal cal;
+	int expected = 1764;
+	int actual = cal.getZegop(42);
+	EXPECT_EQ(expected, actual);
 }
 
 int main() {
