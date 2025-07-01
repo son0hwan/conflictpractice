237cf4adaac111124a8dd40b93fd94e7eb4f1515
sj21.lim <lsj960427@gmail.com>
2025-07-01 15:29:00 +0900

    sumsum

--- Project18/main.cpp
+++ Project18/main.cpp
@@ -3,11 +3,20 @@
 class Cal {
 public:
 	// 이곳에 코드 작성
+
+	int getSumSum(int a, int b, int c) {
+		return a + b + c;
+	}
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, SumSum) {
+	Cal cal;
+	EXPECT_EQ(0, cal.getSumSum(-1, 0, 1));
 }
 
 int main() {
