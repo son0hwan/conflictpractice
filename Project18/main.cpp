e37c27645e16bcd048bfc7edb0a6dc82aef46820
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-01 15:16:54 +0900

    [new] add getsum

--- Project18/main.cpp
+++ Project18/main.cpp
@@ -3,11 +3,17 @@
 class Cal {
 public:
 	// 이곳에 코드 작성
+	int getSum(int a, int b) {
+		return a + b;
+	}
 };
 
 //테스트 케이스 작성
-TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+TEST(CALCULATOR, TC_SUM) {
+	Cal* cal = new Cal();
+	int expect = 5;
+	int actual = cal->getSum(2, 3);
+	EXPECT_EQ(expect, actual);
 }
 
 int main() {
