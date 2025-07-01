43e0264148a946d0123db98b4a4dda29eb25c3d3
sonny <sonny@google.com>
2025-07-01 15:33:02 +0900

    main: divider: return 0 when divisor is 0
    Signed-off-by: sonny <sonny@google.com>

--- Project18/main.cpp
+++ Project18/main.cpp
@@ -3,6 +3,9 @@
 class Cal {
 public:
 	int getDivide(int a, int b) {
+		if (b == 0)
+			return 0;
+
 		return a / b;
 	}
 };
@@ -12,6 +15,12 @@ TEST(TS, TC_DEVIDE) {
 	int expect = 5;
 	int actual = cal.getDivide(10, 2);
 	EXPECT_EQ(expect, actual);
+}
+
+TEST(TS, TC_DEVIDE2) {
+	Cal cal;
+	
+	EXPECT_EQ(0, cal.getDivide(1, 0));
 }
 
 int main() {
