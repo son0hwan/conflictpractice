910950ed3adfdacb15dffdffef3a335ed9abef66
sonny <sonny@google.com>
2025-07-01 14:58:18 +0900

    Initial Commit

---
+++ Project18/main.cpp
@@ -0,0 +1,16 @@
+#include "gmock/gmock.h"
+
+class Cal {
+public:
+	// 이곳에 코드 작성
+};
+
+//테스트 케이스 작성
+TEST(t1, t2) {
+	EXPECT_EQ(1, 1);
+}
+
+int main() {
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
